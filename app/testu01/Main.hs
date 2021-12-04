2f43a45e849cc1c41660f0428ae921d8f6704add
Tazki <tatsuki.devel@gmail.com>
2021-12-04 22:41:40 +0900

    update testu01 main

--- app/testu01/Main.hs
+++ app/testu01/Main.hs
@@ -3,8 +3,9 @@ module Main where
 import Test.HTestU
 import System.Random.Ergodic
 import System.Random
-import Test.HTestU.Streaming (nextStreamFromGen)
-import Test.HTestU.Wrapping  (Battery)
+import System.Environment    ( getArgs )
+import Test.HTestU.Streaming ( nextStreamFromGen )
+import Test.HTestU.Wrapping  ( Battery )
 
 runCrush :: (RandomGen g) => Battery -> IO g -> IO [TestResult]
 runCrush crush gen = (\g -> runBatteryToResults nextStreamFromGen g crush ) `fmap` gen
@@ -22,22 +23,23 @@ runBigCrushErgo :: IO [TestResult]
 runBigCrushErgo =  runCrush c_bigCrush getErgoGen
 
 main :: IO ()
-main =  do rStd <- runSmallCrushStd
-           putStrLn "Standard Small Crush"
-           putStrLn $ show rStd
-           putStrLn "\n"
+main =  do args <- getArgs
+           case (read $ args !! 0) of
+                0 -> do rSCErgo <- runSmallCrushErgo
+                        putStrLn "Ergodic Small Crush"
+                        putStrLn $ show rSCErgo
+                        putStrLn "\n"
+                1 -> do rCErgo <- runCrushErgo
+                        putStrLn "Ergodic Crush"
+                        putStrLn $ show rCErgo
+                        putStrLn "\n"
+                _ -> do rBCErgo <- runBigCrushErgo
+                        putStrLn "Ergodic Big Crush"
+                        putStrLn $ show rBCErgo
+                        putStrLn "\n"
 
-           rSCErgo <- runSmallCrushErgo
-           putStrLn "Ergodic Small Crush"
-           putStrLn $ show rSCErgo
-           putStrLn "\n"
 
-           rCErgo <- runCrushErgo
-           putStrLn "Ergodic Crush"
-           putStrLn $ show rCErgo
-           putStrLn "\n"
-
-           rBCErgo <- runBigCrushErgo
-           putStrLn "Ergodic Big Crush"
-           putStrLn $ show rBCErgo
-           putStrLn "\n"
+           -- rStd <- runSmallCrushStd
+           -- putStrLn "Standard Small Crush"
+           -- putStrLn $ show rStd
+           -- putStrLn "\n"
