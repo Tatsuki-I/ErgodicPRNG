522f6f8d304e623dc508e6fd98e1d185fd3aa209
Tazki <tatsuki.devel@gmail.com>
2021-12-04 16:40:36 +0900

    Update Main.hs

--- app/testu01/Main.hs
+++ app/testu01/Main.hs
@@ -15,12 +15,29 @@ runSmallCrushStd =  runCrush c_smallCrush newStdGen
 runSmallCrushErgo :: IO [TestResult]
 runSmallCrushErgo =  runCrush c_smallCrush getErgoGen
 
+runCrushErgo :: IO [TestResult]
+runCrushErgo =  runCrush c_crush getErgoGen
+
+runBigCrushErgo :: IO [TestResult]
+runBigCrushErgo =  runCrush c_bigCrush getErgoGen
+
 main :: IO ()
 main =  do rStd <- runSmallCrushStd
-           putStrLn "Standard"
+           putStrLn "Standard Small Crush"
            putStrLn $ show rStd
-           rErgo <- runSmallCrushErgo
-           putStrLn "Ergodic"
-           putStrLn $ show rErgo
+           putStrLn "\n"
 
+           rSCErgo <- runSmallCrushErgo
+           putStrLn "Ergodic Small Crush"
+           putStrLn $ show rSCErgo
+           putStrLn "\n"
 
+           rCErgo <- runCrushErgo
+           putStrLn "Ergodic Crush"
+           putStrLn $ show rCErgo
+           putStrLn "\n"
+
+           rBCErgo <- runBigCrushErgo
+           putStrLn "Ergodic Big Crush"
+           putStrLn $ show rBCErgo
+           putStrLn "\n"
